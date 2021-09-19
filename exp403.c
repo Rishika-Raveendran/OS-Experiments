d238bcdfdd4737490ff86331f001089afe61a8d1
Rishika <rishikarocky14@gmail.com>
2021-09-19 18:57:19 +0530

    exp4.3-FCFS

--- exp403.c
+++ exp403.c
@@ -10,30 +10,11 @@ void main()
     printf("\nEnter Burst Time:\n");
     for (i = 0; i < n; i++)
     {
-        printf("p%d:", i + 1);
+        printf("P%d:", i + 1);
         scanf("%d", &bt[i]);
         p[i] = i + 1;
     }
-    //selection sort
-    for (i = 0; i < n; i++)
-    {
-        pos = i;
-        for (j = i + 1; j < n; j++)
-        {
-            if (bt[j] < bt[pos])
-                pos = j;
-        }
 
-        temp = bt[i];
-        bt[i] = bt[pos];
-        bt[pos] = temp;
-
-        temp = p[i];
-        p[i] = p[pos];
-        p[pos] = temp;
-    }
-
-    //wt[0] = 0;
     wt = 0;
     printf("\nProcess\t\tBurst Time\t\tWaiting Time\t\tTurnaround Time");
     for (j = 0; j < n; j++)
@@ -48,7 +29,7 @@ void main()
 
     avg_wt = wt / n;
 
-    avg_tat = (float)total / n; //average turnaround time
+    avg_tat = (float)total / n;
     printf("\n\nAverage Waiting Time=%f", avg_wt);
     printf("\nAverage Turnaround Time=%f\n", avg_tat);
 }
